805ca1771f51422029defd6319151014e323e464
ollie <ollierahman@gmail.com>
2018-03-31 08:43:09 +0100

    Further autogen, implemented table data storage.

--- Incremental-Game/src/resourceEvents.js
+++ Incremental-Game/src/resourceEvents.js
@@ -1,9 +1,9 @@
 function updateResources(resourceType, addition){
-    let res = document.getElementById(resourceType).innerHTML;
-	let currRes = parseFloat(res.split(' ')[1]);
-	let resType = res.substr(0, res.indexOf(':'));
-	let newValue = ((currRes+addition).toString());
-	res.innerHTML = (resType + ": " + newValue);
+    let res = document.getElementById(resourceType);
+	let currRes = resources[resourceType];
+	let newValue = (currRes+addition);
+    resources[resourceType] = newValue;
+	res.innerHTML = (resourceType + ": " + newValue.toString());
 }
 
 
@@ -14,22 +14,56 @@ function testIS(txt){
 
 function createBuyButton(obj){
     let objRef = Items[obj];
-    let ul = document.getElementById(objRef.buyBar);
-    let newBuy = document.createElement("li");
-    let newBuytext = document.createElement("p");
-    let newBuyTT = document.createElement("span");
+    if (objRef.available === 'true'){
+        let ul = document.getElementById(objRef.buyBar);
+        let newBuy = document.createElement('li');
+        let newBuytext = document.createElement('p');
+        let newBuyTT = document.createElement('span');
+        let keys = Object.keys(objRef.cost);
+        let costs = Object.values(objRef.cost);
+
+        newBuyTT.setAttribute('class', 'tooltiptext');
+        newBuyTT.innerHTML = '';
+        for (let i = 0; i < costs.length; i++){
+            newBuyTT.innerHTML += (costs[i] + ' ' + keys[i] +"  ");
+        }
+        newBuytext.innerHTML = objRef.name;
+        newBuy.setAttribute('class', 'actionButton');
+        newBuy.setAttribute('onClick', 'attemptBuy("'+ objRef.name +'")');
+
+        ul.appendChild(newBuy);
+        newBuy.append(newBuytext);
+        newBuytext.append(newBuyTT);
+
+        objRef.available = 'false';
+    }
+}
+
+/*
+function attemptBuy(obj){
+    let objRef = Items[obj];
     let keys = Object.keys(objRef.cost);
     let costs = Object.values(objRef.cost);
-
-    newBuyTT.setAttribute("class", "tooltiptext");
-    newBuyTT.innerHTML = "";
+    let succ;
     for (let i = 0; i < costs.length; i++){
-        newBuyTT.innerHTML += (costs[i] + " " + keys[i] +"  ");
+        let res = document.getElementById(keys[i]).innerHTML;
+        if (costs[res] < parseFloat((res.substring(res.indexOf(' '),res.length)))){
+            succ = true;
+        } else {
+            succ = false;
+            break;
+        }
     }
-    newBuytext.innerHTML = objRef.name;
-    newBuy.setAttribute('class', 'actionButton');
 
-    ul.appendChild(newBuy);
-    newBuy.append(newBuytext);
-    newBuytext.append(newBuyTT);
-}
+    if (succ === true){
+        for (let j = 0; j < costs.length; j++){
+            let res = document.getElementById(keys[j]).innerHTML;
+            let currRes = parseFloat(res.split(' ')[1]);
+            let newValue = ((currRes-costs[res]).toString());
+            res.innerHTML = (resType + ": " + newValue);
+        }
+        updateLog("Got it.");
+    } else {
+        updateLog("Can't afford it.");
+    }*/
+
---
+++ Incremental-Game/src/tableData.js
@@ -0,0 +1,55 @@
+
+let resources = {
+    // collectible resources
+    'electrum'                  : 0,
+    'food'                      : 0,
+    'water'                     : 0,
+    'wood'                      : 0,
+    'metal'                     : 0,
+    'cloth'                     : 0,
+    'electronics'               : 0,
+    'components'                : 0,
+    'high quality components'   : 0,
+    'crystals'                  : 0,
+
+    // mob loot
+    'chitin'                    : 0,
+    'monster flesh'             : 0,
+    'monster eyes'              : 0,
+    'monster skin'              : 0,
+
+    // ammo
+    'makeshift bullet'          : 0,
+    'bullet'                    : 0,
+    'high quality bullet'       : 0
+
+};
+
+let tools = {
+    'hacking tool'  : false
+};
+
+let Items = {
+		'Chisel': {
+			name: 'Chisel',
+			availableMsg: ("Found a broken bit of pipe in the crawlspace. Might be able to find a use for it."),
+			buildMsg: ('Fashioned myself a little chisel. Might be able to get through the wall now.'),
+			buyBar: "darkBuyBar",
+            available: 'true',
+			purchased: 'false',
+                cost : { 'metal': 1,
+                         'cloth': 1
+			}
+		},
+
+		'Repair Console': {
+			name: 'Repair Console',
+			availableMsg: ("This old console is beat up bad. I need some wires to fix it up."),
+			buildMsg: ('Engineer wired the thing back together. Time to try turning it on.'),
+			buyBar: "consoleBuyBar",
+			type: '',
+            cost : {
+                'electronics': 3
+            }
+		}
+    };
